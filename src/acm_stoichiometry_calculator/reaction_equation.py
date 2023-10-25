1f3d952f4554c991f36fe0d8f7965c3a23952a1a
GloriousChonk <mysticalmagic7@outlook.com>
2023-10-24 18:55:32 -0600

    Began equation interpretation

--- src/acm_stoichiometry_calculator/reaction_equation.py
+++ src/acm_stoichiometry_calculator/reaction_equation.py
@@ -8,6 +8,20 @@ class ReactionEquation:
             self.elements.append((name, number))
 
 
-    def __init__(self) -> None:
-        self.reactants = []
-        self.products = []
+    def __init__(self, e_string:str) -> None:
+        self.reactants, self.products = interpret_equation(e_string)
+
+    def interpret_equation(self, e_string:str):
+        e_string.replace(" ", "")
+        react_str, prod_str = e_string.split('=')
+        return(func(react_str), func(prod_str))
+
+    def interpret_one_side(self, side_string:str):
+        molecules = side_string.split('+')
+        for molecule in molecules:
+            element = ""
+            number = ""
+            for c in molecule:
+                if c.isupper() and len(element) != 0:
+                    pass
+                element += c
